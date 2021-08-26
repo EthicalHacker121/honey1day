a8da53490453b34e74861c2be71da98ab38642f7
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-08-26 10:47:02 +0530

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -3,6 +3,6 @@ pkg install git
 pkg install python 
 pkg install figlet
 rm honey.py honey1.py honey*.py
-
+wget -q "https://raw.githubusercontent.com/EthicalHacker121/honey1day/main/honey.py"
 clear
 python hackrxce1.py
