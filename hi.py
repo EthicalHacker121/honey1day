3fa66f51c278e0e12fc496f0b268af0e69df7e8d
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-09-02 10:14:41 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -136,8 +136,8 @@ if(expirydate>today):
     Secondend = now.replace(hour=14, minute=35, second=0, microsecond=0)
     Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
     Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
-    Final = now.replace(hour=17, minute=55, second=0, microsecond=0)
-    Finalend = now.replace(hour=18, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=19, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=20, minute=35, second=0, microsecond=0)
 
     if (False):
             period=220
